9cb179d2b26112370e6723ebdac900071431133c
shimislm <shimislm5@gmail.com>
2022-11-03 08:30:19 +0200

    fix some connection errors

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const cors = require("cors");
-const { routesInit } = require("./routes/config_routes")
+const { routesInit} = require("./routers/config_routes")
 require("./db/mongoconnect");
 
 const app = express();
 
-// נותן גישה לכל הדומיינים לגשת לשרת שלנו
 app.use(cors());
-// כדי שנוכל לקבל באדי
+
 app.use(express.json());
-// הגדרת תקיית הפאבליק כתקייה ראשית
+
 app.use(express.static(path.join(__dirname, "public")))
 
 routesInit(app);
--- db/mongoconnect.js
+++ db/mongoconnect.js
@@ -4,6 +4,6 @@ const { config } = require('../config/config');
 main().catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.vvexxth.mongodb.net/test`);
+    await mongoose.connect(`mongodb+srv://${config.userDb}:${config.passDb}@cluster0.vvexxth.mongodb.net/RentInout`);
     console.log("mongo connect...")
 }
