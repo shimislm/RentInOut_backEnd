da811d21ef8dc544a84a58f3654a16e713eddcd2
shimislm <shimislm5@gmail.com>
2022-11-03 09:13:13 +0200

    user schema start

--- models/userModel.js
+++ models/userModel.js
@@ -1,18 +1,68 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+
+const prodObj = {
+  user_id: String,
+  renatl_date: Date,
+  title: String,
+  img: String,
+  category: String,
+};
+const massageObj = {sentDate:{
+    type: Date,
+    default: Date.now(),
+}};
+const chatObj = {
+  self: { name: String, data: String, user_id: String },
+  other: { name: String, data: String, user_id: String },
+  messeges_ar: { 
+    type: [massageObj],
+    default: [] },
+};
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    phone: String,
-    role: {
-        type: String,
-        default: "user"
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    }
-})
+  fullName: {
+    firstName: String,
+    LastName: String,
+  },
+  email: String,
+  password: String,
+  phone: String,
+  profile_img: String,
+  cover_img: String,
 
-exports.UserModel = mongoose.model('users', userSchema);
+  role: {
+    type: String,
+    default: "user",
+  },
+  date_created: {
+    type: Date,
+    default: Date.now(),
+  },
+  birtdate: Date,
+  active: Boolean,
+  renk: Number,
+  bio: String,
+  productsList: {
+    type: [String],
+    default: [],
+  },
+  location: String,
+  rented_products: {
+    type: [prodObj],
+    default: [],
+  },
+  clicked_products: {
+    type: [prodObj],
+    default: [],
+  },
+  liked_products: {
+    type: [prodObj],
+    default: [],
+  },
+  chat_ar: {
+    type: [chatObj],
+    default: [],
+  },
+});
+
+exports.UserModel = mongoose.model("users", userSchema);
