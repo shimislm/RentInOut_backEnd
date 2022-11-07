fb696d84ad68c50bb387aa6434cd035ff2989d83
Shay <shaybush93@gmail.com>
2022-11-07 19:28:42 +0200

    checkLikes done

--- routers/posts.js
+++ routers/posts.js
@@ -12,9 +12,10 @@ router.get("/countMyPosts", auth, postCtrl.countMyPosts);
 router.patch("/changeActive/:postID", authAdmin, postCtrl.changeActive);
 router.get("/single/:postID", auth ,postCtrl.singleInfo);
 router.patch("/changeRange/:postID", auth, postCtrl.changeRange);
-// not done yet
 router.get("/search", postCtrl.search);
-router.get("/checkLikes/:postID");
+router.get("/checkLikes/:postID",postCtrl.countLikes);
+// not done/ not working yet
+router.post("/likePost/:postID",auth,  postCtrl.likePost)
 router.get("/topThreeLikes/:postID");
 
 module.exports = router;
