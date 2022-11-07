6dd0c85944f52d39c9113b3a124aeaf3ccf38b02
Shay <shaybush93@gmail.com>
2022-11-07 09:09:28 +0200

    validation done

--- models/postModel.js
+++ models/postModel.js
@@ -1,36 +1,48 @@
 
 const mongoose = require("mongoose");
 const likesObject = {
-    user_id : String,
+    user_id: String,
     profile: String,
-    fullName : {
-        firstName:String,
-        lastName:String
+    fullName: {
+        firstName: String,
+        lastName: String
     }
 }
 const postSchema = new mongoose.Schema({
-    name : String,
-    info : String,
-    img : String,
-    range :{
+    postName: String,
+    info: String,
+    img: String,
+    range: {
         type: String,
-        enum: ['long-term','short-term'],
-        default : 'short-term'
+        enum: ['long-term', 'short-term'],
+        default: 'short-term'
     },
-    creator_id : String,
-    category_url : String,
-    price : Number,
-    type : {
+    creator_id: String,
+    category_url: String,
+    price: Number,
+    type: {
         type: String,
-        enum: ['rent','exchange','delivery'],
-        default : 'rent' 
+        enum: ['rent', 'exchange', 'delivery'],
+        default: 'rent'
     },
-    collect_points : Array(String),
-    active : Boolean,
-    available_from : Date,
-    location : String,
-    likes: Array(likesObject)
+    collect_points: {
+        type : Array(String),
+        default : []
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    available_from: {
+        type: Date,
+        default: Date.now()
+    },
+    location: String,
+    likes:{
+        type: Array(likesObject),
+        default: []
+    }
 },
-{timestamps: true});
+    { timestamps: true });
 
 exports.UserModel = mongoose.model("users", userSchema);
