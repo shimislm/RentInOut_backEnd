67b9b5bf0cfb64905b89a7faa0d3cdcb3c803a79
Shay <shaybush93@gmail.com>
2022-11-09 15:05:44 +0200

    download new modules for google login

--- config/config.js
+++ config/config.js
@@ -7,5 +7,7 @@ exports.config = {
     userDb:process.env.USER_DB,
     passDb:process.env.PASS_DB,
     tokenSecret:process.env.TOKEN_SECRET,
-    superID: process.env.ADMIN_ID
+    superID: process.env.ADMIN_ID,
+    googleId : process.env.GOOGLE_CLIENT_ID,
+    googleSecret : process.env.GOOGLE_CLIENT_SECRET
   }
