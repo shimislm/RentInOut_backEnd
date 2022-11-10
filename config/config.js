098cdad3fe0c5113a60e0e99d6bee327e2c85e8d
Shay <shaybush93@gmail.com>
2022-11-10 13:14:10 +0200

    update

--- config/config.js
+++ config/config.js
@@ -7,7 +7,5 @@ exports.config = {
     userDb:process.env.USER_DB,
     passDb:process.env.PASS_DB,
     tokenSecret:process.env.TOKEN_SECRET,
-    superID: process.env.ADMIN_ID,
-    googleId : process.env.GOOGLE_CLIENT_ID,
-    googleSecret : process.env.GOOGLE_CLIENT_SECRET
+    superID: process.env.ADMIN_ID
   }
