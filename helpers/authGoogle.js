8df864c8badb2cbf6d076ba71a681104ec4d8f9a
Shay <shaybush93@gmail.com>
2022-11-10 15:44:11 +0200

    login with google works!

--- helpers/authGoogle.js
+++ helpers/authGoogle.js
@@ -11,7 +11,7 @@ passport.use(new GoogleStrategy({
   passReqToCallback: true
 },
   function (request, accessToken, refreshToken, profile, done) {
-    return done(profile);
+    return done(null,profile);
   }
 ));
 
--- routers/users.js
+++ routers/users.js
@@ -5,48 +5,66 @@ const { userCtrl } = require("../controllers/userCtrl");
 const { authCtrl } = require("../controllers/authCtrl");
 const passport = require("passport");
 
+
+//google signIn
+router.get('/auth/google',passport.authenticate('google', {
+    scope: ['email', 'profile']
+}))
+router.get('/welcome', function (req, res) {
+    if (req.session.loggedin) {
+        res.redirect("../test.html")
+    } else {
+        res.json({err : "access denied"});
+        res.end();
+    }
+});
+router.get('/google/callback', passport.authenticate('google', {
+    failureRedirect: "/",
+}),(req,res) =>{
+    req.session.userData = {
+        profile: req.user.photos[0].value,
+        email: req.user.email,
+        user : req.user
+    };
+    req.session.loggedin = true;
+    req.googleUser = req.user.email;
+    res.redirect("/users/welcome");
+    res.end();
+});
+router.get('/getData', function (req, response) {
+    if (req.session.loggedin) {
+        var data = JSON.stringify(req.session.userData);
+        response.write(data);
+        response.end();
+    } else {
+        response.write("access denied");
+        response.end();
+    }
+});
+router.get('/logout', function (req, response) {
+    if (req.session.loggedin) {
+        req.session.destroy();
+        response.redirect("/");
+        response.end()
+    } else {
+        response.redirect("/");
+        response.end()
+    }
+});
 // authonication routes
 router.post('/', authCtrl.signUp)
-
-
 router.get("/verified" , authCtrl.verifiedUser)
-
 router.get("/verify/:userId/:uniqueString", authCtrl.verifyUser)
-
 router.post('/login', authCtrl.login)
-
-router.get('/auth/google', passport.authenticate('google',
-    { scope: ['email', 'profile'] }));
-router.get( '/google/callback',
-passport.authenticate( 'google', {
-    successRedirect: '/protected',
-    failureRedirect: '/auth/google/failure'
-}));
-router.get('/auth/google/failure', async(req,res) =>{
-    res.json({err:"something went wrong"})
-  })
-router.get('/protected', isLoggedIn, async(req,res) =>{
-    res.json({msg:"hello friend"})
-})
-
 router.post("/requestPasswordReset", authCtrl.requestPasswordReset)
-
 router.post("/resetPassword" , authCtrl.resetPassword)
-
 // user routes
-
 router.get("/userList", authAdmin, userCtrl.getUsersList)
-
 router.get("/countUsers", authAdmin, userCtrl.countUsers)
-
 router.get('/info/:id', auth, userCtrl.infoById)
-
 router.put("/:idEdit", auth, userCtrl.edit)
-
 router.delete("/:idDel", auth, userCtrl.delete)
-
 router.patch("/changeRole/:userID", authAdmin, userCtrl.changeRole)
-
 router.patch("/changeActive/:userID", authAdmin, userCtrl.changeActive)
 
 
