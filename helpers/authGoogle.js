11d8d83114f925c22a374e2f2f4e09b4492ea10c
shimislm <shimislm5@gmail.com>
2022-11-13 14:49:35 +0200

    gmail login issue

--- helpers/authGoogle.js
+++ helpers/authGoogle.js
@@ -1,13 +1,14 @@
 const passport = require('passport');
+const { config } = require("../config/config");
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 const GOOGLE_CLIENT_ID = "808903375990-as0s17c8ks56jt5b57c66rks3m6dj4rd.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
 
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/users/google/callback",
+  clientID: config.google_client_id,
+  clientSecret: config.google_client_secret,
+  callbackURL: config.domain+"/users/google/callback",
   passReqToCallback: true
 },
   function (request, accessToken, refreshToken, profile, done) {
