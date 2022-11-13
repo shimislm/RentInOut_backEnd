5bfc30a6da5be48b2ffce62b5b8e0f6e5642c865
Shay <shaybush93@gmail.com>
2022-11-13 15:15:13 +0200

    update

--- helpers/passport.js
+++ helpers/passport.js
@@ -1,8 +1,11 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const GOOGLE_CLIENT_ID = "808903375990-as0s17c8ks56jt5b57c66rks3m6dj4rd.apps.googleusercontent.com"
 const GOOGLE_CLIENT_SECRET = "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"
+const FACEBOOK_APP_ID = ""
+const FACEBOOK_APP_SECRET = ""
 
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
@@ -13,6 +16,17 @@ passport.use(new GoogleStrategy({
   function (request, accessToken, refreshToken, profile, done) {
     return done(null,profile);
   }
+));
+passport.use(new FacebookStrategy({
+  clientID: FACEBOOK_APP_ID,
+  clientSecret: FACEBOOK_APP_SECRET,
+  callbackURL: "http://localhost:3000/users/facebook/callback"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
 ));
 
 passport.serializeUser(function (user, done) {
