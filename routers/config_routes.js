e67ba9c8280beb295db0e066be40104116acbf82
shimislm <shimislm5@gmail.com>
2022-11-13 13:36:19 +0200

    bug fix

--- routers/config_routes.js
+++ routers/config_routes.js
@@ -3,7 +3,6 @@ const usersR = require("./users");
 const postR = require("./posts");
 const categoriesR = require("./categories");
 const uploadsR = require("./uploads");
-const veriifyR = require("./verification");
 
 exports.routesInit = (app) => {
     app.use("/", indexR);
@@ -11,5 +10,4 @@ exports.routesInit = (app) => {
     app.use("/posts", postR);
     app.use("/categories", categoriesR);
     app.use("/uploads", uploadsR);
-    app.use("/verification", veriifyR)
 }
