04ca6db87f0934089aa52565c23ea1898d6c3c8e
shimislm <shimislm5@gmail.com>
2022-11-20 10:43:55 +0200

    logout done

--- controllers/logoutCtrl.js
+++ controllers/logoutCtrl.js
@@ -3,33 +3,29 @@ const { UserModel } = require("../models/userModel");
 
 const handleLogout = async (req, res) => {
   const cookies = req.cookies;
+  console.log(cookies.jwt)
   //need to delet access token in client side as well
   try {
     if (!cookies?.jwt || cookies.jwt === "" || cookies.jwt === null || cookies.jwt === undefined) {
       return res.sendStatus(204);
     }
     const refreshToken = cookies.jwt;
-
+    
     const user = await UserModel.findOne({ refreshToken }).exec();
-
+    
     if (!user) {
       res.clearCookie("jwt", {
         httpOnly: true,
         sameSite: "None",
-        secure: true,
       });
       return res.status(204).json({msg :"Cookie deleted successfully."});
     }
     // delete in DB
     user.refreshToken = ''
     const result = await user.save();
-    console.log(result);
-
-    res.clearCookie("jwt", {
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
-    });
+    alert("cleared")
+    
+    res.clear("jwt");
     return res.status(204).send("Cookie deleted successfully.");
   } catch (e) {
     return res.status(204).send("Please authenticate.");
