41a1af2b45c5853cfd84bfeed478da2b1a6c6188
shimislm <shimislm5@gmail.com>
2022-11-25 17:22:07 +0200

    mail verification rearange

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -74,96 +74,69 @@ exports.authCtrl = {
 
   verifyUser: async (req, res) => {
     let { userId, uniqueString } = req.params;
-    UserVerificationModel.findOne({ userId })
-      .then((result) => {
+    try {
+      let user = await UserVerificationModel.findOne({ userId })
         //check if user exist in system
-        if (result) {
+        if (user) {
           const { expiresAt } = result;
           const hashedUniqueString = result.uniqueString;
           if (expiresAt < Date.now() + 2 * 60 * 60 * 1000) {
             //checkes if link expired and sent a messege, delete verify collection in db
-            UserVerificationModel.deleteOne({ userId })
-              .then(() => {
-                UserModel.deleteOne({ _id: userId })
-                  .then(() => {
+            try{
+              let del = await UserVerificationModel.deleteOne({ userId })
+              if(del) {
+                  UserModel.deleteOne({ _id: userId }).then(() => {
                     let message = "link has expired.please sigh up again ";
-                    res.redirect(
-                      `/users/verified/?error=true&message=${message}`
-                    );
+                    res.redirect(`/users/verified/?error=true&message=${message}`);
                   })
-                  .catch(() => {
-                    let message =
-                      "clearing user with expired unique string failed ";
-                    res.redirect(
-                      `/users/verified/?error=true&message=${message}`
-                    );
-                  });
-              })
-              .catch((error) => {
-                console.log(error);
-                let message =
-                  "an error occurre while clearing  expired user verification record";
+              }else{
+                    let message = "clearing user with expired unique string failed ";
+                    res.redirect(`/users/verified/?error=true&message=${message}`);
+                  };
+            }catch(error){
+                let message ="an error occurre while clearing  expired user verification record";
                 res.redirect(`/users/verified/?error=true&message=${message}`);
-              });
+              };
           } else {
-            bcrypt
-              .compare(uniqueString, hashedUniqueString)
-              .then((result) => {
-                if (result) {
+            let result = await bcrypt.compare(uniqueString, hashedUniqueString)
+              if(result){
+                try{
                   // update user to active state
-                  UserModel.updateOne({ _id: userId }, { active: true })
-                    .then(() => {
+                  let update = await UserModel.updateOne({ _id: userId }, { active: true })
+                    if(update){
                       // delet verify user collection when verified
-                      UserVerificationModel.deleteOne({ userId })
-                        .then(() => {
-                          res.sendFile(
-                            path.join(__dirname, "./../views/verified.html")
-                          );
-                        })
-                        .catch((error) => {
-                          console.log(error);
-                          let message =
-                            "an error occurre while finalizing sucssful verification  ";
-                          res.redirect(
-                            `/users/verified/?error=true&message=${message}`
-                          );
-                        });
-                    })
-                    .catch((error) => {
-                      console.log(error);
-                      let message =
-                        "an error occurre while updating user verified ";
-                      res.redirect(
-                        `/users/verified/?error=true&message=${message}`
-                      );
-                    });
-                } else {
-                  let message =
-                    "invalid verification details passed.check your inbox.";
-                  res.redirect(
-                    `/users/verified/?error=true&message=${message}`
-                  );
+                      let del = await UserVerificationModel.deleteOne({ userId })
+                        if(del){
+                          res.sendFile(path.join(__dirname, "./../views/verified.html"));
+                        }
+                        else{
+                          let message ="an error occurre while finalizing sucssful verification";
+                          res.redirect(`/users/verified/?error=true&message=${message}`)
+                        };
+                    }else{
+                      let message ="an error occurre while updating user verified ";
+                      res.redirect(`/users/verified/?error=true&message=${message}`);
+                    };
+                }catch {
+                  let message ="invalid verification details passed.check your inbox.";
+                  res.redirect(`/users/verified/?error=true&message=${message}`);
                 }
-              })
-              .catch((error) => {
-                console.log(error);
-                let message =
-                  "an error occurre while compering unique strings ";
+                //couldnt verify unique string
+              }else{
+                let message ="an error occurre while compering unique strings ";
                 res.redirect(`/users/verified/?error=true&message=${message}`);
-              });
+              };
           }
         } else {
           let message =
             "Account doesnt exist or has been verified already.please sign up or login in.";
           res.redirect(`/users/verified/?error=true&message=${message}`);
         }
-      })
-      .catch((error) => {
-        console.log(error);
+    }catch (error) {
         let message =
           "an error occurre while checking for existing user Verification record ";
         res.redirect(`/users/verified/?error=true&message=${message}`);
-      });
+      };
   },
   verifiedUser: async (req, res) => {
     res.sendFile(path.join(__dirname, "../views/verified.html"));
@@ -174,20 +147,13 @@ exports.authCtrl = {
       if (data) {
         // check if user is active
         if (!data.active) {
-          res.json({
-            status: "failed",
-            message:
-              "Email isn't verified yet or account as been suspended, please check your email",
-          });
+          res.json({status: "failed", message:"Email isn't verified yet or account as been suspended, please check your email"});
         } else {
           // procced to email reset pasword
           sendResetEmail(data, redirectUrl, res);
         }
       } else {
-        res.json({
-          status: "failed",
-          message: "No account with the supplied email found. Please try again",
-        });
+        res.json({status: "failed", message: "No account with the supplied email found. Please try again"});
       }
     });
   },
