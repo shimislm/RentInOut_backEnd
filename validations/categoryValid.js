b63e11eb872928664fc8efbc73c172c601ea0336
shimislm <shimislm5@gmail.com>
2022-11-28 09:52:31 +0200

    remove img url from validate category

--- validations/categoryValid.js
+++ validations/categoryValid.js
@@ -5,7 +5,6 @@ exports.validateCategory = (_reqBody) => {
       name:Joi.string().min(2).max(99).required(),
       url_name:Joi.string().min(2).max(99).required(),
       info:Joi.string().min(2).max(500).required(),
-      img_url:Joi.string().min(2).max(200).required()
     })
     return joiSchema.validate(_reqBody);
   }
